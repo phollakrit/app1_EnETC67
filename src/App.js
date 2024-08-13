aa4f6cf2d6470864c738bad63bad254ca90255c1
pat123 <phollakrit@hotmail.com>
2024-08-13 15:05:20 +0700

    add banner
    add banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Calendar from './class-components'
 
+
 export default function App(){
   return(
-    <>
-      <p><center><Calendar/></center></p>
-    </>
+    <Banner/>
   )
 }
 
