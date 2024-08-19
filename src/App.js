0def493ca1aa3c9c11deb0328a40beb739d42d7b
pat123 <phollakrit@hotmail.com>
2024-08-19 10:16:09 +0700

    Create refs-func.js and update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { EventData2 } from './event-data'
-
-
+import RefsFunc from './refs-func'
 
 export default function App(){
   return(
-    <EventData2/>
+    <RefsFunc/>
   )
 }
 
