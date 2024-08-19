5284b5ca12c388332e5584fcee92f2bc59a3d8f1
pat123 <phollakrit@hotmail.com>
2024-08-19 11:38:46 +0700

    Create state-func.js and update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import RefsFunc from './refs-func'
+import MessageBox from './state-func'
 
 export default function App(){
   return(
-    <RefsFunc/>
+    <MessageBox/>
   )
 }
 
