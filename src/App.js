a313b634391c456dda0e5f421bc532fffe378e45
pat123 <phollakrit@hotmail.com>
2024-09-02 11:44:56 +0700

    Add context
    Add context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import Effect from './effect'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
+
 
 
 export default function App(){
+  let [user, setUser]=React.useState('')
   return(
-    <Effect/>
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
   )
 }
 
